b39ccf8e81e34e69a91a5eb51898342d430d9a88
OmegaK2 <omegak2@gmx.de>
2015-08-03 02:18:56 +0200

    - forgot graphviz in setup.py (needed for some of the CLI stuff) - updated the specification

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyside', 'configobj', 'colorama'],
+    install_requires=['pyside', 'configobj', 'colorama', 'graphviz'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
