f7eccdb0dbb7a76835292a8c41750030a4d6220e
OmegaK2 <omegak2@gmx.de>
2015-09-05 16:27:55 +0200

    Added the possibility to pass messages to deprecation deco

--- PyPoE/shared/decorators.py
+++ PyPoE/shared/decorators.py
@@ -42,17 +42,29 @@ __all__ = ['deprecated']
 # Classes
 # =============================================================================
 
+class DeprecationDecorator():
+    def __init__(self, message='Use of %(func)s is deprecated'):
+        self.message = message
+
+    def __call__(self, function):
+        @functools.wraps(function)
+        def deprecated_function(*args, **kwargs):
+            warnings.warn(
+                self.message % {
+                    'func': function.__name__,
+                }, DeprecationWarning
+            )
+
+            return function(*args, **kwargs)
+
+        return deprecated_function
+
 # =============================================================================
 # Functions
 # =============================================================================
 
-def deprecated(function):
-    @functools.wraps(function)
-    def deprecated_function(*args, **kwargs):
-        warnings.warn(
-            'Use of %s is deprecated' % function.__name__, DeprecationWarning
-        )
-
-        return function(*args, **kwargs)
-
-    return deprecated_function
+def deprecated(*args, **kwargs):
+    if len(args) == 1 and callable(args[0]):
+        return DeprecationDecorator()(args[0])
+    else:
+        return DeprecationDecorator(*args, **kwargs)
--- tests/PyPoE/shared/test_decorators.py
+++ tests/PyPoE/shared/test_decorators.py
@@ -1,7 +1,7 @@
 """
 Path     tests/PyPoE/shared/test_decorators.py
 Name     Tests for PyPoE.shared.decorators
-Version  
+Version  1.0.0a0
 Revision $Id$
 Author   Omega_K2
 
@@ -12,7 +12,7 @@ INFO
 
 AGREEMENT
 
-
+See PyPoE/LICENSE
 
 
 TODO
@@ -44,12 +44,24 @@ from PyPoE.shared import decorators
 # Tests
 # =============================================================================
 
-def test_deprecated():
+class TestDeprecated:
+    def test_simple_call(self):
+        @decorators.deprecated
+        def temp(x):
+            pass
 
-    @decorators.deprecated
-    def temp(x):
-        pass
+        pytest.deprecated_call(temp, 5)
 
-    pytest.deprecated_call(temp, 5)
+    def test_empty_args(self):
+        @decorators.deprecated()
+        def temp(x):
+            pass
 
+        pytest.deprecated_call(temp, 5)
 
+    def test_message_args(self):
+        @decorators.deprecated(message='Test')
+        def temp(x):
+            pass
+
+        pytest.deprecated_call(temp, 5)
