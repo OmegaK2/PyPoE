f9531d6309c545ec1674e822b62fb7ce6b3ca1ce
OmegaK2 <omegak2@gmx.de>
2015-11-28 08:22:03 +0100

    Fixes and improvements to decorators
    decorators.py:
    - doc decorator no longer wraps, so it's possible to apply it to any object
    - fixed issue with not setting docstrings with certain object types
    - deprecated decorator adds deprecation message to the doc string of the object automatically; argument  doc_message added for this
    - deprecated decorator default improved to include the version in which the object will be removed; argument version added for this
    - improved the docstrings
    
    test_decorators.py:
    - updated test cases to test for doc decorator
    - added more automated tests for various object types

--- PyPoE/shared/decorators.py
+++ PyPoE/shared/decorators.py
@@ -41,23 +41,70 @@ import warnings
 # Globals
 # =============================================================================
 
-__all__ = ['deprecated']
+__all__ = ['deprecated', 'doc']
+
 
 # =============================================================================
 # Classes
 # =============================================================================
 
+
 class DeprecationDecorator(object):
-    def __init__(self, message='Use of %(func)s is deprecated'):
-        self.message = message
+    """
+    Decorator for setting functions as deprecated.
+
+    Applying this decorate will cause DeprecationWarnings to be emitted on
+    function call and also adds the DEPRECATED
+
+    """
+    _default_message = 'Use of {func} is deprecated and will be removed in' \
+                       ' PyPoE {version}'
+    _default_doc_message = 'DEPRECATED. Will be removed in PyPoE {version}'
+
+    def __init__(self, message=None, doc_message=None, version=None):
+        """
+        Settings for the decorator.
+
+        Both message and doc_message argument accept the following message
+        formatter strings:
+        +---------+------------------------------------------------------------+
+        | version | Version the function will be removed in                    |
+        +---------+------------------------------------------------------------+
+
+        Additionally, message accepts the following message formatter string:
+        +------+---------------------------------------------------------------+
+        | func | The name of the function                                      |
+        +------+---------------------------------------------------------------+
+
+        :param str message: Warning message on each call.
+        :param str doc_message: Message to append to docstring
+        :param str version: Version the function will be removed in
+        """
+        self.message = message or self._default_message
+        self.doc_message = doc_message or self._default_doc_message
+        self.version = version or 'unknown version'
 
     def __call__(self, function):
+        message_kwargs = {
+            'version': self.version,
+        }
+
+        if hasattr(function, '__func__'):
+            function = function.__func__
+
+        if function.__doc__ is None:
+            function.__doc__ = self.doc_message.format(**message_kwargs)
+        else:
+            function.__doc__ = self.doc_message.format(**message_kwargs) + \
+                               '\n' + function.__doc__
+
         @functools.wraps(function)
         def deprecated_function(*args, **kwargs):
             warnings.warn(
-                self.message % {
-                    'func': function.__name__,
-                }, DeprecationWarning, stacklevel=2,
+                self.message.format(
+                    func=function.__name__,
+                    **message_kwargs
+                ), DeprecationWarning, stacklevel=2,
             )
 
             return function(*args, **kwargs)
@@ -68,13 +115,17 @@ class DeprecationDecorator(object):
 class DocStringDecorator(object):
     """
     Decorator for docstring modifications.
+
+    It will modify the doc string of a given object, but will not actually
+    wrap it. This is done so it can work with any type of object.
     """
+
     def __init__(self, prepend='', append='', doc=None):
         """
         :param prepend: String to prepend to the docstring
         :type prepend: str
 
-        :param append: String to append to the doctsting
+        :param append: String to append to the docstring
         :type append: str
 
         :param doc: Docstring to use. If an object that doesn't inherit from str
@@ -86,12 +137,8 @@ class DocStringDecorator(object):
         self.doc = doc
 
     def __call__(self, function):
-        @functools.wraps(function)
-        def deprecated_function(*args, **kwargs):
-            return function(*args, **kwargs)
-
         if self.doc is None:
-            docs = deprecated_function.__doc__
+            docs = function.__doc__
         elif isinstance(self.doc, str):
             docs = self.doc
         else:
@@ -103,13 +150,18 @@ class DocStringDecorator(object):
         docs = self.prepend + docs + self.append
 
         if docs != '':
-            deprecated_function.__doc__ = docs
+            if hasattr(function, '__func__'):
+                function.__func__.__doc__ = docs
+            else:
+                function.__doc__ = docs
 
-        return deprecated_function
+        return function
+
 
 # =============================================================================
 # Functions
 # =============================================================================
+
 
 def _make_callable(cls):
     @functools.wraps(cls)
@@ -118,7 +170,13 @@ def _make_callable(cls):
             return cls()(args[0])
         else:
             return cls(*args, **kwargs)
+
     return call
+
+
+# =============================================================================
+# Init
+# =============================================================================
 
 deprecated = _make_callable(DeprecationDecorator)
 doc = _make_callable(DocStringDecorator)
--- tests/PyPoE/shared/test_decorators.py
+++ tests/PyPoE/shared/test_decorators.py
@@ -30,6 +30,7 @@ See PyPoE/LICENSE
 # =============================================================================
 
 # Python
+import sys
 
 # 3rd-party
 import pytest
@@ -38,35 +39,129 @@ import pytest
 from PyPoE.shared import decorators
 
 # =============================================================================
+# Utility functions
+# =============================================================================
+
+def make_test_list(stuff):
+    parsed_data = []
+    for decorator, kwargs in stuff:
+        row = []
+        if kwargs is None:
+            row.append(decorator)
+        else:
+            row.append(decorator(**kwargs))
+
+        parsed_data.append(row)
+
+
+# =============================================================================
 # Setup
 # =============================================================================
 
-# =============================================================================
-# Fixtures
-# =============================================================================
+
+def make_objects(uncallables=False):
+    def function(*args, **kwargs):
+        pass
+    
+    class A:
+        """A"""
+        def __call__(self, *args, **kwargs):
+            """__call__"""
+            pass
+    
+        def method(self, *args, **kwargs):
+            """method"""
+            pass
+    
+        @classmethod
+        def classmethod(cls, *args, **kwargs):
+            """classmethod"""
+            pass
+
+    data = [
+        # callable object, instance object
+        (function, None),
+        (A.classmethod, None),
+        (A.method, A),
+        (A.__call__, A),
+    ]
+
+    if uncallables:
+        data.append((A, None))
+
+    return data
 
 # =============================================================================
 # Tests
 # =============================================================================
 
+
 class TestDeprecated:
-    def test_simple_call(self):
-        @decorators.deprecated
-        def temp(x):
-            pass
+    def run_test(self, decorator, callobj, obj):
+        with pytest.warns(DeprecationWarning) as record:
+            o = decorator(callobj)
+            args = (1, 2, 3)
+            kwargs = {'a': 1, 'b': 2, 'c': 3}
+            if obj:
+                o(obj(), *args, **kwargs)
+            else:
+                o(*args, **kwargs)
+            return record
 
-        pytest.deprecated_call(temp, 5)
+    @pytest.mark.parametrize('callobj,obj', make_objects())
+    def test_simple(self, callobj, obj):
+        self.run_test(decorators.deprecated, callobj, obj)
 
-    def test_empty_args(self):
-        @decorators.deprecated()
-        def temp(x):
-            pass
+    @pytest.mark.parametrize('callobj,obj', make_objects())
+    def test_empty_args(self, callobj, obj):
+        self.run_test(decorators.deprecated(), callobj, obj)
 
-        pytest.deprecated_call(temp, 5)
+    @pytest.mark.parametrize('callobj,obj', make_objects())
+    def test_message_arg(self, callobj, obj):
+        deco = decorators.deprecated(message='Test')
 
-    def test_message_args(self):
-        @decorators.deprecated(message='Test')
-        def temp(x):
-            pass
+        for warn in self.run_test(deco, callobj, obj):
+            assert warn.message.args[0] == 'Test'
 
-        pytest.deprecated_call(temp, 5)
+    @pytest.mark.parametrize('callobj,obj', make_objects())
+    def test_message_arg(self, callobj, obj):
+        deco = decorators.deprecated(doc_message='Test')
+
+        self.run_test(deco, callobj, obj)
+        assert callobj.__doc__.startswith('Test')
+
+
+class TestDoc:
+    """Test"""
+
+    @pytest.mark.parametrize('callobj,obj', make_objects(True))
+    def test_simple(self, callobj, obj):
+        o = decorators.doc(callobj)
+
+    @pytest.mark.parametrize('callobj,obj', make_objects(True))
+    def test_empty_args(self, callobj, obj):
+        o = decorators.doc()(callobj)
+
+    @pytest.mark.parametrize('callobj,obj', make_objects(True))
+    def test_arg_prepend(self, callobj, obj):
+        o = decorators.doc(prepend='Test')(callobj)
+
+        assert o.__doc__.startswith('Test')
+
+    @pytest.mark.parametrize('callobj,obj', make_objects(True))
+    def test_arg_append(self, callobj, obj):
+        o = decorators.doc(append='Test')(callobj)
+
+        assert o.__doc__.endswith('Test')
+
+    @pytest.mark.parametrize('callobj,obj', make_objects(True))
+    def test_arg_doc_with_str(self, callobj, obj):
+        o = decorators.doc(doc='Test')(callobj)
+
+        assert o.__doc__ == 'Test'
+
+    @pytest.mark.parametrize('callobj,obj', make_objects(True))
+    def test_arg_doc_with_obj(self, callobj, obj):
+        o = decorators.doc(doc=TestDoc)(callobj)
+
+        assert o.__doc__ == 'Test'
