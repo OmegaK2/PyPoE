b528d719c490604808529908642f42d796944420
OmegaK2 <omegak2@gmx.de>
2016-02-26 02:25:50 +0100

    Added a IntEnumValidator generator, docstrings and tests

--- PyPoE/shared/config/validator.py
+++ PyPoE/shared/config/validator.py
@@ -19,6 +19,20 @@ Description
 
 Additional validators for configobj and validate.
 
+Validator Function Generators
+-------------------------------------------------------------------------------
+
+.. autoclass:: IntEnumValidator
+
+  .. automethod:: __call__
+
+Validator Functions
+-------------------------------------------------------------------------------
+
+.. autofunction:: is_file
+
+.. autofunction:: is_directory
+
 Agreement
 ===============================================================================
 
@@ -31,6 +45,7 @@ See PyPoE/LICENSE
 
 # Python
 import os
+from enum import IntEnum
 
 # 3rd Party
 from validate import ValidateError, is_boolean
@@ -39,11 +54,120 @@ from validate import ValidateError, is_boolean
 # Globals
 # =============================================================================
 
-__all__ = ['is_directory', 'is_file', 'functions']
+__all__ = ['IntEnumValidator', 'is_directory', 'is_file', 'functions']
+
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+
+class IntEnumValidator(object):
+    """
+    Class to create a dynamic validator for IntEnum classes
+    """
+    def __init__(self, enum, default=None):
+        """
+
+        Parameters
+        ----------
+        enum : :py:class:`enum.IntEnum`
+            base enum class for this validator
+        default : :py:class:`enum.IntEnum` | int | str | None
+            default attribute of the enum
+        Raises
+        ------
+        TypeError
+            if enum is not an :py:class:`enum.IntEnum` class
+        TypeError
+            if default parameter is of invalid type
+
+        """
+        if not issubclass(enum, IntEnum):
+            raise TypeError('enum must be an IntEnum subclass')
+        self._enum = enum
+
+        if default is None:
+            self._default = None
+        elif isinstance(default, enum):
+            self._default = default
+        elif isinstance(default, int):
+            self._default = enum(int)
+        elif isinstance(default, str):
+            self._default = getattr(enum, default)
+        else:
+            raise TypeError('default must be a subtype of default')
+        self._default = None
+
+    def _get_enum_from_val(self, value):
+        """
+        Parameters
+        ----------
+        value : object
+            the value to pass to the enum class from class creation
+
+        Returns
+        -------
+        IntEnum
+            IntEnum instance based on the class upon class creation
+
+        Raises
+        ------
+        ValidateError
+            if the the value isn't a member of the IntEnum class
+        """
+        try:
+            return self._enum(value)
+        except ValueError:
+            raise ValidateError(
+                '%s The value is not accepted by the enum.' %
+                self._enum.__name__
+            )
+
+    def __call__(self, value):
+        """
+        Parameters
+        ----------
+        value : object
+            The value to validate
+
+        Returns
+        -------
+        IntEnum
+            the int enum instance if successfully validated or None
+
+        Raises
+        ------
+        ValidateError
+            if the the value can't be validated
+        """
+        if isinstance(value, str):
+            try:
+                value = int(value)
+            except ValueError:
+                try:
+                    value = getattr(self._enum, value)
+                except AttributeError:
+                    raise ValidateError(
+                        'The value is neither an integer or a valid %s ' \
+                        'attribute' % self._enum.__name__
+                    )
+            else:
+                value = self._get_enum_from_val(value)
+        elif isinstance(value, int):
+            value = self._get_enum_from_val(value)
+        elif value is None:
+            return self._default
+        else:
+            raise ValidateError('Invalid type')
+
+        return value
+
 
 # =============================================================================
 # Functions
 # =============================================================================
+
 
 def _exists(value, exists):
     if not isinstance(exists, bool):
@@ -52,13 +176,57 @@ def _exists(value, exists):
     if exists and not os.path.exists(value):
         raise ValidateError('Path "%s" does not exist.' % value)
 
+
 def is_file(value, *args, exists=True, **kwargs):
+    """
+    Checks whether the value is a valid file path (and optionally whether it
+    exists).
+
+    Parameters
+    ----------
+    value : object
+        The value to validate
+    exists : bool
+        whether the file is required to exist to pass the validation check
+
+    Returns
+    -------
+    IntEnum
+        the int enum instance if successfully validated or None
+
+    Raises
+    ------
+    ValidateError
+        if the the value can't be validated
+    """
     _exists(value, exists)
     if not os.path.isfile(value):
         raise ValidateError('"%s" is not a file.' % value)
     return value
-    
+
+
 def is_directory(value, *args, exists=True, **kwargs):
+    """
+    Checks whether the value is a valid directory path (and optionally whether
+    it exists).
+
+    Parameters
+    ----------
+    value : object
+        The value to validate
+    exists : bool
+        whether the directory is required to exist to pass the validation check
+
+    Returns
+    -------
+    IntEnum
+        the int enum instance if successfully validated or None
+
+    Raises
+    ------
+    ValidateError
+        if the the value can't be validated
+    """
     _exists(value, exists)
     if not os.path.isdir(value):
         raise ValidateError('"%s" is not a directory.' % value)
---
+++ tests/PyPoE/shared/config/test_validator.py
@@ -0,0 +1,121 @@
+"""
+Overview
+===============================================================================
+
++----------+------------------------------------------------------------------+
+| Path     | test_validator.py                                                |
++----------+------------------------------------------------------------------+
+| Version  | 1.0.0a0                                                          |
++----------+------------------------------------------------------------------+
+| Revision | $Id$                                                             |
++----------+------------------------------------------------------------------+
+| Author   | Omega_K2                                                         |
++----------+------------------------------------------------------------------+
+
+Description
+===============================================================================
+
+
+
+Agreement
+===============================================================================
+
+See PyPoE/LICENSE
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Python
+import os
+from enum import IntEnum
+
+# 3rd-party
+import pytest
+from validate import ValidateError
+
+# self
+from PyPoE.shared.config import validator
+
+# =============================================================================
+# Setup
+# =============================================================================
+
+
+class MyIntEnum(IntEnum):
+    a = 1
+    b = 2
+    c = 3
+
+# =============================================================================
+# Fixtures
+# =============================================================================
+
+@pytest.fixture(scope='module')
+def int_enum_tester():
+    return validator.IntEnumValidator(enum=MyIntEnum, default=MyIntEnum.a)
+
+@pytest.fixture(scope='module')
+def file_path():
+    return __file__
+
+
+@pytest.fixture(scope='module')
+def dir_path():
+    return os.path.split(__file__)[0]
+
+
+# =============================================================================
+# Tests
+# =============================================================================
+
+class TestIntEnumValidator:
+    values = (
+        (1, MyIntEnum.a),
+        ('a', MyIntEnum.a),
+        ('1', MyIntEnum.a),
+    )
+
+    fvalues = (
+        ('4', '4 (str) is out of range and should fail'),
+        ('2.0', '2.0 (str) is not a valid integer'),
+        (4, '4 (int) is out of range and should fail'),
+        (2.0, '2.0 (float) is not a valid integer'),
+    )
+
+
+    def test_init(self, int_enum_tester):
+        with pytest.raises(TypeError):
+            validator.IntEnumValidator(enum=int)
+            validator.IntEnumValidator(enum=MyIntEnum, default=5)
+            validator.IntEnumValidator(enum=MyIntEnum, default='c')
+            validator.IntEnumValidator(enum=MyIntEnum, default=int)
+
+    @pytest.mark.parametrize('value,expected', values)
+    def test_success(self, int_enum_tester, value, expected):
+        assert int_enum_tester(value) == expected
+
+    @pytest.mark.parametrize('value,errmsg', fvalues)
+    def test_success(self, int_enum_tester, value, errmsg):
+        with pytest.raises(ValidateError):
+            int_enum_tester(value)
+
+
+
+class TestIsFile:
+    def test_success(self, file_path):
+        assert validator.is_file(file_path) == file_path
+
+    @pytest.mark.xfail(ValidateError)
+    def test_fail(self, dir_path):
+        validator.is_file(dir_path)
+
+
+class TestIsDirectory:
+    def test_success(self, dir_path):
+        assert validator.is_directory(dir_path) == dir_path
+
+    @pytest.mark.xfail(ValidateError)
+    def test_fail(self, file_path):
+        validator.is_directory(file_path)
