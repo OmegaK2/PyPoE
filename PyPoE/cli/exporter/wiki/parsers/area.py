ab5377e3b16f1920d4d9ada443e1e9059715f0fb
OmegaK2 <omegak2@gmx.de>
2017-06-11 12:15:31 +0200

    Added a WIP/prototype area exporter for the wiki

---
+++ PyPoE/cli/exporter/wiki/parsers/area.py
@@ -0,0 +1,372 @@
+"""
+Overview
+===============================================================================
+
++----------+------------------------------------------------------------------+
+| Path     | PyPoE/cli/exporter/wiki/parsers/area.py                          |
++----------+------------------------------------------------------------------+
+| Version  | 1.0.0a0                                                          |
++----------+------------------------------------------------------------------+
+| Revision | $Id$                  |
++----------+------------------------------------------------------------------+
+| Author   | Omega_K2                                                         |
++----------+------------------------------------------------------------------+
+
+Description
+===============================================================================
+
+
+
+Agreement
+===============================================================================
+
+See PyPoE/LICENSE
+
+Documentation
+===============================================================================
+
+Public API
+-------------------------------------------------------------------------------
+
+Interal API
+-------------------------------------------------------------------------------
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Python
+from functools import partialmethod
+from collections import OrderedDict
+
+# 3rd-party
+
+# self
+from PyPoE.cli.core import console, Msg
+from PyPoE.cli.exporter.wiki.handler import ExporterHandler, ExporterResult, \
+    add_format_argument
+from PyPoE.cli.exporter.wiki.parser import BaseParser, format_result_rows
+
+# =============================================================================
+# Globals
+# =============================================================================
+
+__all__ = []
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+
+class AreaCommandHandler(ExporterHandler):
+    def __init__(self, sub_parser):
+        self.parser = sub_parser.add_parser(
+            'area',
+            help='Area Exporter',
+        )
+        self.parser.set_defaults(func=lambda args: self.parser.print_help())
+
+        sub = self.parser.add_subparsers()
+
+        # By id
+        a_id = sub.add_parser(
+            'id',
+            help='Extract areas by their id.'
+        )
+        self.add_default_parsers(
+            parser=a_id,
+            cls=AreaParser,
+            func=AreaParser.by_id,
+        )
+        add_format_argument(a_id)
+        a_id.add_argument(
+            'area_id',
+            help='Id of the area, can be specified multiple times.',
+            nargs='+',
+        )
+
+        # by name
+        a_name = sub.add_parser(
+            'name',
+            help='Extract areas by their name.'
+        )
+        self.add_default_parsers(
+            parser=a_name,
+            cls=AreaParser,
+            func=AreaParser.by_name,
+        )
+        add_format_argument(a_name)
+        a_name.add_argument(
+            'area_name',
+            help='Visible name of the area (localized), can be specified multiple times.',
+            nargs='+',
+        )
+
+        # by row ID
+        a_rid = sub.add_parser(
+            'rowid',
+            help='Extract areas by rowid.'
+        )
+        self.add_default_parsers(
+            parser=a_rid,
+            cls=AreaParser,
+            func=AreaParser.by_rowid,
+        )
+        add_format_argument(a_rid)
+        a_rid.add_argument(
+            'start',
+            help='Starting index',
+            nargs='?',
+            type=int,
+            default=0,
+        )
+        a_rid.add_argument(
+            'end',
+            nargs='?',
+            help='Ending index',
+            type=int,
+        )
+
+
+class AreaParser(BaseParser):
+    _files = [
+        'WorldAreas.dat'
+    ]
+
+    _area_column_index_filter = partialmethod(
+        BaseParser._column_index_filter,
+        dat_file_name='WorldAreas.dat',
+        error_msg='Several areas have not been found:\n%s',
+    )
+
+    _COPY_KEYS = OrderedDict((
+        ('Id', {
+            'template': 'id',
+        }),
+        ('Name', {
+            'template': 'name',
+        }),
+        ('Act', {
+            'template': 'act',
+        }),
+        ('AreaLevel', {
+            'template': 'level',
+        }),
+        ('MaxLevel', {
+            'template': 'level_restriction_max',
+            'default': 100,
+        }),
+        ('AreaType_TagsKeys', {
+            'template': 'area_type_tags',
+            'format': lambda value: ', '.join([
+                tag['Id'] for tag in value
+            ]),
+            'default': [],
+        }),
+        ('TagsKeys', {
+            'template': 'tags',
+            'format': lambda value: ', '.join([
+                tag['Id'] for tag in value
+            ]),
+            'default': [],
+        }),
+        ('LoadingScreen_DDSFile', {
+            'template': 'loading_screen',
+            'format': lambda value: value.replace('Art/Textures/Interface/Loadi'
+                'ngImages/', '').replace('.dds', ''),
+        }),
+        ('Connections_WorldAreasKeys', {
+            'template': 'connections',
+            'format': lambda value: ', '.join([
+                area['Id'] for area in value
+            ]),
+            'default': [],
+        }),
+        ('ParentTown_WorldAreasKey', {
+            'template': 'parent_town',
+            'format': lambda value: value['Id'],
+        }),
+        ('ModsKeys', {
+            'template': 'mod_ids',
+            'format': lambda value: ', '.join([
+                mod['Id'] for mod in value
+            ]),
+            'default': [],
+        }),
+        ('Bosses_MonsterVarietiesKeys', {
+            'template': 'boss_monster_ids',
+            'format': lambda value: ', '.join([
+                mv['Id'] for mv in value
+            ]),
+            'default': [],
+        }),
+        ('Monsters_MonsterVarietiesKeys', {
+            'template': 'monster_ids',
+            'format': lambda value: ', '.join([
+                mv['Id'] for mv in value
+            ]),
+            'default': [],
+        }),
+        ('FirstEntry_NPCTextAudioKey', {
+            'template': 'entry_text',
+            'format': lambda value: value['Text'],
+        }),
+        ('FirstEntry_NPCsKey', {
+            'template': 'entry_npc',
+            'condition': lambda area:
+                area['FirstEntry_NPCTextAudioKey'] is not None,
+            'format': lambda value: value['Name'],
+        }),
+        # Spawn chances section
+        ('VaalArea_SpawnChance', {
+            'template': 'vaal_area_spawn_chance',
+            'condition': lambda area: area['VaalArea_SpawnChance'] > 0 and
+                                      area['VaalArea_WorldAreasKeys'],
+        }),
+        ('VaalArea_WorldAreasKeys', {
+            'template': 'vaal_area_ids',
+            'condition': lambda area: area['VaalArea_SpawnChance'] > 0 and
+                                      area['VaalArea_WorldAreasKeys'],
+            'format': lambda value: ', '.join([
+                area['Id'] for area in value
+            ]),
+        }),
+
+        ('Strongbox_SpawnChance', {
+            'template': 'strongbox_spawn_chance',
+            'condition': lambda area: area['Strongbox_SpawnChance'] > 0,
+        }),
+        ('Strongbox_MaxCount', {
+            'template': 'strongbox_max',
+            'condition': lambda area: area['Strongbox_SpawnChance'] > 0,
+            'default': 0,
+        }),
+        ('Strongbox_RarityWeight', {
+            'template': 'strongbox_rarity_weight',
+            'condition': lambda area: area['Strongbox_SpawnChance'] > 0,
+            'default': '',
+            'format': lambda value: ', '.join([str(v) for v in value]),
+        }),
+        # bools
+        ('IsMapArea', {
+            'template': 'is_map',
+            'default': False,
+        }),
+        ('IsUniqueMapArea', {
+            'template': 'is_unique_map_area',
+            'default': False,
+        }),
+        ('IsTown', {
+            'template': 'is_town',
+            'default': False,
+        }),
+        ('IsHideout', {
+            'template': 'is_hideout',
+            'default': False,
+        }),
+        ('IsVaalArea', {
+            'template': 'is_vaal_area',
+            'default': False,
+        }),
+        ('IsMasterDailyArea', {
+            'template': 'is_master_daily_area',
+            'default': False,
+        }),
+        ('IsLabyrinthArea', {
+            'template': 'is_labyrinth_area',
+            'default': False,
+        }),
+        ('IsLabyrinthAirlock', {
+            'template': 'is_labyrinth_airlock',
+            'default': False,
+        }),
+        ('IsLabyrinthBossArea', {
+            'template': 'is_labyrinth_boss_area',
+            'default': False,
+        }),
+        ('HasWaypoint', {
+            'template': 'has_waypoint',
+            'default': False,
+        }),
+    ))
+
+    def by_rowid(self, parsed_args):
+        return self.export(
+            parsed_args,
+            self.rr['WorldAreas.dat'][parsed_args.start:parsed_args.end],
+        )
+
+    def by_id(self, parsed_args):
+        return self.export(parsed_args, self._area_column_index_filter(
+            column_id='Id', arg_list=parsed_args.area_id
+        ))
+
+    def by_name(self, parsed_args):
+        return self.export(parsed_args, self._area_column_index_filter(
+            column_id='Name', arg_list=parsed_args.area_name
+        ))
+
+    def export(self, parsed_args, areas):
+        console('Found %s areas, parsing...' % len(areas))
+
+        r = ExporterResult()
+
+        if areas:
+            console('Found %s areas. Processing...' % len(areas))
+        else:
+            console(
+                'No areas found for the specified parameters. Quitting.',
+                msg=Msg.warning,
+            )
+            return r
+
+        for area in areas:
+            data = OrderedDict()
+
+            for row_key, copy_data in self._COPY_KEYS.items():
+                value = area[row_key]
+
+                condition = copy_data.get('condition')
+                if condition is not None and not condition(area):
+                    continue
+
+                # Skip default values to reduce size of template
+                if value == copy_data.get('default'):
+                    continue
+                '''default = copy_data.get('default')
+                if default is not None and value == default:
+                        continue'''
+
+                fmt = copy_data.get('format')
+                if fmt:
+                    value = fmt(value)
+                data[copy_data['template']] = value
+
+            for i, (tag, value) in enumerate(zip(area['SpawnWeight_TagsKeys'],
+                                                 area['SpawnWeight_Values'])):
+                data['spawn_weight%s_tag' % i] = tag['Id']
+                data['spawn_weight%s_value' % i] = value
+
+            r.add_result(
+                text=format_result_rows(
+                    parsed_args=parsed_args,
+                    template_name='Area',
+                    ordered_dict=data,
+                ),
+                out_file='area_%s.txt' % data['id'],
+                wiki_page=[
+                    {
+                        'page': 'Area:' + self._format_wiki_title(data['id']),
+                        'condition': None
+                    },
+                ],
+                wiki_message='Mod updater',
+            )
+
+
+        return r
+
+# =============================================================================
+# Functions
+# =============================================================================
---
+++ PyPoE/poe/sim/monster.py
@@ -0,0 +1,191 @@
+"""
+Utilities for dealing with monsters and related calculations.
+
+Overview
+===============================================================================
+
++----------+------------------------------------------------------------------+
+| Path     | PyPoE/poe/sim/monster.py.                                        |
++----------+------------------------------------------------------------------+
+| Version  | 1.0.0a0                                                          |
++----------+------------------------------------------------------------------+
+| Revision | $Id$                  |
++----------+------------------------------------------------------------------+
+| Author   | Omega_K2                                                         |
++----------+------------------------------------------------------------------+
+
+Description
+===============================================================================
+
+Utilities for dealing with monsters and related calculations.
+
+Agreement
+===============================================================================
+
+See PyPoE/LICENSE
+
+Documentation
+===============================================================================
+
+Public API
+-------------------------------------------------------------------------------
+
+Interal API
+-------------------------------------------------------------------------------
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Python
+from collections import Iterable
+
+# 3rd-party
+
+# self
+from PyPoE.poe.constants import RARITY
+from PyPoE.poe.file import RelationalReader, OTFileCache
+
+# =============================================================================
+# Globals
+# =============================================================================
+
+__all__ = []
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+
+class Monster():
+    def __init__(self, parent, mv):
+        self.parent = parent
+        self._mv = mv
+        self._mt = self.mv['MonsterTypesKey']
+        self._res = self.mt['MonsterResistancesKey']
+        self._ge = self.mv['GrantedEffectsKeys']
+        self._gepl = [gepl for gepl in parent.rr['GrantedEffectsPerLevel.dat']
+                     if gepl['GrantedEffectsKey'] == self._ge]
+        self._mods = self.mv['ModsKeys']
+
+        self._level = None
+
+    @property
+    def level(self):
+        if self._level is None:
+            raise ValueError('Set monster level first before performing actions'
+                             ' that require monster level')
+        return self._level
+
+    @level.setter
+    def level(self, value):
+        if value < 1 or value > 100:
+            raise ValueError('Monster level must be 1-100')
+        self._level = value
+
+    def damage(self, map_tier=None):
+        base = self.parent.rr['DefaultMonsterStats.dat'][self.level]
+
+
+class MonsterFactory():
+    """
+    Handler for monsters.
+    """
+
+    _files = [
+        'DefaultMonsterStats.dat',
+        # Loads mods, stats, granted effects, etc
+        'MonsterVarieties.dat',
+        'GrantedEffectsPerLevel.dat',
+    ]
+
+    rarity_mods = None
+
+    def __init__(self, *args, relational_reader, otfile_cache, **kwargs):
+        """
+
+        Parameters
+        ----------
+        relational_reader : RelationalReader
+            info
+        otfile_cache : OTFileCache
+            info
+        """
+        if isinstance(relational_reader, RelationalReader):
+            self.rr = relational_reader
+        else:
+            raise ValueError('relational_reader must be a RelationalReader '
+                             'instance')
+        if isinstance(otfile_cache, OTFileCache):
+            self.ot = otcache
+        else:
+            raise ValueError('otfile_cache must be a OTFileCache instance.')
+
+        # Load files
+        for fn in self._files:
+            self.rr[fn]
+
+        self.rarity_mods = {
+            RARITY.NORMAL: [],
+        }
+        for mod in self.rr['Mods.dat']:
+            if mod['Id'].startswith('MonsterMagic'):
+                self.rarity_mods[RARITY.MAGIC] = mod
+            elif mod['Id'].startswith('MonsterRare'):
+                self.rarity_mods[RARITY.RARE] = mod
+            elif mod['Id'].startswith('MonsterUnique'):
+                self.rarity_mods[RARITY.UNIQUE] = mod
+
+    def monster(self, rowid=None, metaid=None, name=None, *args, **kwargs):
+        """
+        Creates a list of Monster instances and returns them based on the
+        passed search parameters.
+
+        Parameters
+        ----------
+        rowid : int or Iterable[int]
+            rowid or list of rowids to search for
+        metaid : str or Iterable[str]
+            Metadata id or list of metadata ids to search for
+        name : str or Iterable[str]
+            Name or list of monster names to search for
+        args : Iterable
+            Extra positional arguments to pass to the Monster instance
+        kwargs : dict
+            Extra keyword arguments to pass to the Monster instance
+
+        Returns
+        -------
+        list[Monster]
+            List of Monster instances.
+        """
+        if isinstance(rowid, int):
+            mv = [self.rr['MonsterVarieties.dat'][rowid], ]
+        elif isinstance(rowid, Iterable):
+            mv = [self.rr['MonsterVarieties.dat'][rid] for rid in rowid]
+        elif isinstance(metaid, str):
+            mv = [self.rr['MonsterVarieties.dat'].index['Id'][metaid], ]
+        elif isinstance(metaid, Iterable):
+            mv = [self.rr['MonsterVarieties.dat'].index['Id'][mid] for mid
+                  in metaid]
+        elif isinstance(name, str):
+            mv = [m for m in self.rr['MonsterVarieties.dat'] if
+                  m['Name'] == name]
+        elif isinstance(name, Iterable):
+            mv = [m for m in self.rr['MonsterVarieties.dat'] if
+                  m['Name'] in name]
+        else:
+            raise ValueError('One of rowid, metaid or name must be specified '
+                             'and be of the correct type')
+
+        return [Monster(
+            *args,
+            parent=self,
+            mv=m,
+            **kwargs
+        ) for m in mv]
+
+# =============================================================================
+# Functions
+# =============================================================================
