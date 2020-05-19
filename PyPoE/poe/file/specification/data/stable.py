8ecbf8eb3f3271b4f85aebb3a12bae00235f1d49
Sören Thomas <7596346+brather1ng@users.noreply.github.com>
2020-05-19 21:14:13 +0200

    Fix key of 'Mastery_PassiveSkillsKey' in PassiveTreeExpansionSkills.dat
    A typo snuck into my previous PR

--- PyPoE/poe/file/specification/data/stable.py
+++ PyPoE/poe/file/specification/data/stable.py
@@ -15637,7 +15637,7 @@ specification = Specification({
             ('Mastery_PassiveSkillsKey', Field(
                 name='Mastery_PassiveSkillsKey',
                 type='ulong',
-                key='Mastery_PassiveSkills.dat',
+                key='PassiveSkills.dat',
             )),
             ('TagsKey', Field(
                 name='TagsKey',
